10c448efe09394d847823652f59a072fedf7f92d
meiyuw <meiyu.physics@gmail.com>
2020-02-18 16:10:55 -0500

    A script to setup environment variables/paths.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+source /project/projectdirs/desi/software/desi_environment.sh
+export DESI_MWS_root='/global/homes/m/meiyuw/desi_MWS_pipeline'
+export GRID_DIR=$DESI_MWS_root/piferre
+export rvspecfit_config=$DESI_MWS_root/rvspecfit/desi
+export PATH=$PATH:/global/homes/m/meiyuw/.local/bin
+export DESI_MWS_output='/global/project/projectdirs/desi/users/meiyuw/mws_output'
+export DESI_MWS_script_output='/global/project/projectdirs/desi/users/meiyuw/mws_output/script_output'
